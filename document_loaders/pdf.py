856636e6f885c77b6e8c464b8b1259b4f1d37696
Safan Dar <safandar2@gmail.com>
2026-06-02 16:04:16 +0530

    Recursive text splitting

--- document_loaders/pdf.py
+++ document_loaders/pdf.py
@@ -1,16 +1,19 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import TokenTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 #Reading document
 data = PyPDFLoader("document_loaders/case_study_2327114.pdf")
+
+'''
 text_splitter = TokenTextSplitter(
     chunk_size = 1000,
     chunk_overlap = 100
 )
+
+'''
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=200)
 #Loading Data
 docs = data.load()
 chunks = text_splitter.split_documents(docs)
 
-for i in chunks:
-    print(i.page_content)
-    print("-------------")
-    print("-------------")
+print(chunks[5].page_content)
