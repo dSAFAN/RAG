b2723458338c804bc74edb4248008d2b3986bf03
Safan Dar <safandar2@gmail.com>
2026-06-02 12:48:46 +0530

    used text loaders with chat templates to feed text files

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from langchain_mistralai.chat_models import ChatMistralAI
+from langchain_community.document_loaders import TextLoader
+from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 load_dotenv()
 
-llm = ChatMistralAI(
-    model = "mistral-small-2603"
+llm = ChatMistralAI(model = "mistral-small-2603")
+data = TextLoader("document_loaders/Prerequisite.txt")
+docs = data.load()
+
+template = ChatPromptTemplate.from_messages(
+    [("system","You are an AI Agent that summarizes any given data"),
+     ("human","{data}")]
 )
 
-response = llm.invoke("Hey, you there?")
-print(response)
+final_prompt = template.format_messages(data = docs[0].page_content)
+
+
+response = llm.invoke(final_prompt)
+print(response.content)
