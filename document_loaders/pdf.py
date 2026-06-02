8dc0669cf9e2a091c53cf62487fb04e3d70dac66
Safan Dar <safandar2@gmail.com>
2026-06-02 15:27:30 +0530

    token based text splitting

--- document_loaders/pdf.py
+++ document_loaders/pdf.py
@@ -1,9 +1,16 @@
 from langchain_community.document_loaders import PyPDFLoader
-
+from langchain_text_splitters import TokenTextSplitter
 #Reading document
 data = PyPDFLoader("document_loaders/case_study_2327114.pdf")
-
+text_splitter = TokenTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 100
+)
 #Loading Data
 docs = data.load()
+chunks = text_splitter.split_documents(docs)
 
-print(docs[1])
+for i in chunks:
+    print(i.page_content)
+    print("-------------")
+    print("-------------")
