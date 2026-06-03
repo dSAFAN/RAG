266278fcde8c33e69c4ad3add9aaebba15836800
Safan Dar <safandar2@gmail.com>
2026-06-03 10:43:41 +0530

    DB and Embeddings Creation

--- vector_store/db.py
+++ vector_store/db.py
@@ -1,19 +1,33 @@
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import chroma 
+from langchain_community.vectorstores import Chroma 
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
 load_dotenv()
 
+# Text Splitter
 splitter = RecursiveCharacterTextSplitter(
     chunk_size = 1000,
     chunk_overlap = 200
 )
 
+# Embedding Model
+embedding = HuggingFaceEmbeddings(
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+)
 
+
+# Pdf Loader
 pdf_data = PyPDFLoader("document_loaders/case_study_2327114.pdf")
 docs_pdf = pdf_data.load()
+
+# Chunk creation
 chunks = splitter.split_documents(docs_pdf)
 
-print(len(chunks))
+# embedding and db creation
+vectorstore = Chroma.from_documents(
+    documents = chunks,
+    embedding = embedding,
+    persist_directory = "chromaDB"
+)
